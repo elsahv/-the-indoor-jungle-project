5b5468df504c6e26e7ebe335b24a2970fd8921ed
Elsa <elsahvy@gmail.com>
2023-02-27 22:31:13 -0800

    changed global font to poppins

--- components/Global.js
+++ components/Global.js
@@ -5,7 +5,7 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: 'Cormorant Garamond', serif;
+    font-family: 'Teko', sans-serif;
     scroll-behavior: smooth;
     
   }
