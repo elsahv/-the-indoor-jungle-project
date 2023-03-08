9ae990ced727845ebe6a66534eee34338f0b52ce
Elsa <elsahvy@gmail.com>
2023-03-07 18:42:58 -0800

    hydration error attempt fix

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,36 +1,29 @@
-import { Section, Grid, ContactFormWrapper, Content } from "./styled";
-import ContactForm from "./EmailForm";
+import { Section, Grid, Content } from "./styled";
 
-function FooterSection() {
+function Contact() {
   return (
     <>
       <Section id="contact">
         <Grid>
-          <Content id="sq">
-            <div>
-              <h2>What do you think of this site so far?</h2>
-              <p>{"I'd"} love to hear your feedback!</p>
-              <p>
-                If you are hiring any web developers, {"I'd"} really love to
-                hear from ya!
-              </p>
-              <br />
-              <p>
-                Simply fill out the contact form, or email me at
-                <div style={{ fontWeight: "bold" }}>
-                  devdesignsbyelsa@gmail.com
-                </div>
-              </p>
-            </div>
+          <Content>
+            <h2>What do you think of this site so far?</h2>
+            <p>{"I'd"} love to hear your feedback!</p>
+            <p>
+              If you are hiring any web developers, {"I'd"} really love to hear
+              from ya!
+            </p>
+            <br />
+            <p>
+              Simply fill out the contact form, or email me at
+              <div style={{ fontWeight: "bold" }}>
+                devdesignsbyelsa@gmail.com
+              </div>
+            </p>
           </Content>
-
-          {/* <ContactFormWrapper id="sq"> */}
-          {/* <ContactForm /> */}
-          {/* </ContactFormWrapper> */}
         </Grid>
       </Section>
     </>
   );
 }
 
-export default FooterSection;
+export default Contact;
--- components/Contact/styled.js
+++ components/Contact/styled.js
@@ -30,27 +30,6 @@ export const Grid = styled.div`
   }
 `;
 
-export const ContactFormWrapper = styled.div`
-  grid-area: right;
-  // border: 1px solid #000;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  padding: 100px 0 50px;
-  background: teal;
-  border-radius: 5px;
-
-  @media only screen and (max-width: 1024px) {
-    border-top: 1px solid #000;
-    justify-content: flex-start;
-    align-items: flex-start;
-    padding-left: 50px;
-    padding-top: 20px;
-  }
-  @media only screen and (max-width: 531px) {
-    padding: 10px;
-  }
-`;
 export const Content = styled.div`
   // color: teal;
   // text-shadow: 1px 1px 1px maroon;
